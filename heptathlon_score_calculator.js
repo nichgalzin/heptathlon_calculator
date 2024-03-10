660885eb89449de82915b5c8379c12d10a61f382
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2024-03-10 12:51:52 +0000

    Function to parse data to object form

--- heptathlon_score_calculator.js
+++ heptathlon_score_calculator.js
@@ -1,22 +1,41 @@
-const fs = require('fs');
-const csvFilePath = process.argv[2];
+import { readFile } from 'fs/promises';
 
-//Main function to calculate score
-const heptathlonScoreCalculator = () => {
-  readCSVFile(csvFilePath);
+const main = async () => {
+  const csvFilePath = process.argv[2];
+  const fileContentsInRows = await readInputFile(csvFilePath);
+  const eventData = parseEventData(fileContentsInRows);
+
+  console.log({ eventData });
 };
 
 // Read csv file and returns raw data
-const readCSVFile = (filePath) => {
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error reading the file:', err);
-      return;
-    }
+const readInputFile = async (filePath) => {
+  try {
+    const data = await readFile(filePath, 'utf8');
+    return data.split('\n');
+  } catch (err) {
+    console.error('Error reading the file:', err);
+    throw err;
+  }
+};
 
-    return data;
-  });
+const parseEventData = (rowData) => {
+  return rowData
+    // Remove white space
+    .filter((row) => row.trim() !== '')
+    // Loop through and destructure each entry to a name variable
+    .map((row) => {
+      const [athlete, event, performance, dateTime] = row
+        .split(',')
+      // Return an array of objects with each object representing a row.   
+      return {
+        athlete: athlete.toLowerCase(), 
+        event: event.toLowerCase(), 
+        performance: performance.toLowerCase(), 
+        dateTime: dateTime,
+      };
+    });
 };
 
 //Runs main function
-heptathlonScoreCalculator();
+main();
